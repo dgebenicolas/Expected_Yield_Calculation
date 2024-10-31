910549c2608642d0465354484b98750371193343
Dgebe N <Dgebe N@oagr.olzhaagro.kz>
2024-10-31 10:46:14 +0500

    k

--- app.py
+++ app.py
@@ -75,7 +75,8 @@ def main():
             pre_process_df = rename_product_groups(pre_process_df)
             processed_data = preprocessor.transform(pre_process_df)
             processed_df = pd.DataFrame(processed_data, columns=feature_names)
-            print(processed_df.columns)
+            st.write("Feature Names Used for Prediction:")
+            st.write(processed_df.columns.tolist())
             y_pred = model.predict(processed_df)
             y_pred_list.append(y_pred)
         
